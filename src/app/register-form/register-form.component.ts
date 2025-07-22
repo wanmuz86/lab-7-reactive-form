9de050bd6a6bf159c6ac55ba8307a9e6a1dcc626
Wan Muzaffar <wanmuz86@gmail.com>
2025-07-22 14:21:39 +0800

    add async validation

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AsyncValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { of,delay,map, Observable } from 'rxjs';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register-form',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,NgIf],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.css'
 })
@@ -17,7 +19,13 @@ export class RegisterFormComponent implements OnInit{
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
-      username:[''],
+      username:['' , {
+        // Adding validation to the username FormControl
+        validators: [Validators.required, Validators.minLength(3)],
+        // asyncValidators = Perform validation asynchronously upon onblur (losing focus)
+        asyncValidators: [this.usernameTakenValidator()],
+        updateOn: 'blur' // Update the value on blur 
+      }], // Username is a string
       // User can have multiple emails,
       // Build it as FormArray
       emails: this.formBuilder.array([
@@ -25,6 +33,25 @@ export class RegisterFormComponent implements OnInit{
       ]),
     
     })
+  }
+  // This is a placeholder for the async validator function
+  usernameTakenValidator() : AsyncValidatorFn {
+    // List of taken usernames for simulation
+    const takenUsernames = ['admin','user1','user2'];
+    // THe validation process,  control here refers to the FormControl where the validation will be performed
+    return (control:AbstractControl) => {
+      return of(
+        // if the name entered by user (control.value) is in the array
+        // pipe is used to perform operations on the observable one after another
+
+        takenUsernames.includes(control.value)).pipe
+        (
+          delay(1000), // Simulate API call, wait 1 second to see the loading
+          // Map the boolean value to a ValidationError object, null means ok
+          map(isTaken =>   isTaken ? { usernameTaken: true } : null)
+        )
+      
+    }
   }
 
   onSubmit(){
